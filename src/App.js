c281cc395754659e2bad3c65209545180935c410
Patrick O'Regan <patrick.oregan@pricerunner.com>
2022-08-10 14:33:38 +0200

    continually resize

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes, { sub_routes } from "./routes";
 import Header from "./components/Header";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [height, setHeight] = useState(window.visualViewport.height - 40);
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      setHeight(window.visualViewport.height - 40)
+    );
+
+    return () =>
+      window.removeEventListener(
+        "resize",
+        setHeight(window.visualViewport.height - 40)
+      );
+  }, []);
   return (
-    <div
-      className="App"
-      style={{ minHeight: window.visualViewport.height - 40 }}
-    >
+    <div className="App" style={{ minHeight: height }}>
       <BrowserRouter>
         <Header />
         <div
